4ec968ed4c0e4e8be59a3a0650b430ed063ac70f
Pete Russell <pinkbatz@gmail.com>
2016-10-13 15:27:58 -0700

    Adds a switch/case to the wx reducer, uses spread operator to update state.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import ReduxPromise from 'redux-promise';
 import App from './components/app';
 import reducers from './reducers';
 
+/* ReduxPromise is a middleware. Middleware sits between the action creators
+   and reducers, and intercepts the actions sent from the creators. It can
+   then inspect and remove, modify, allow it to pass, or trigger some other
+   action (kind of like iptables or some kind of proxy).
+
+   Specifically, ReduxPromise will check the 'payload:' field of an action,
+   and if it sees that field's value is a promise, it will stop the action
+   from being dispatched to reducers until the promise is fulfilled.
+
+   Once the request finishes, it dispatches a *new* action of the same type,
+   but with a payload of the *resolved request*. */
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,16 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      /* Remember, we must always return *new* state from a Reducer, we can't
+         mutate existing state and then return that.
+
+         The '...' syntax means expand the array, so in this case we're saying
+         'create a new array, include the new piece of data, then expand the
+         existing state and add each entry to the new array'. */
+      return [ state.payload.data, ...state ];
+  }
+
   return state;
 }
